d85c21858600a88e9a00f91626d058b71b2c3401
Jack Quinoa <jquinoa208@gmail.com>
2020-06-15 22:20:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,18 +16,18 @@ class Triangle
     def kind
          if @one <= 0 || @two <= 0 || @three <= 0
             begin
-                raise TriangleError => error
-                puts error.message
+                raise TriangleError
+                  message
             end
         elsif @one == nil || @two == nil || @three == nil
             begin
-                raise TriangleError => error 
-                puts error.message
+                raise TriangleError 
+                  message
             end
         elsif @one + @two <= @three || @one + @three <= @two || @three + @two <= @one
             begin
-                raise TriangleError => error
-                puts error.message
+                raise TriangleError
+                  message
             end
         end
         
