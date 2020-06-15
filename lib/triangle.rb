091b6f09074fb1ae890a9f96bde31e025887e815
Jack Quinoa <jquinoa208@gmail.com>
2020-06-15 22:17:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -73,20 +73,3 @@ class Triangle
         end
     end
 end
-#     def illegal_triangle
-#         if one <= 0 || two <= 0 || three <= 0
-#             begin
-#                 raise TriangleError
-#             end
-#         elsif one == nil || two == nil || three == nil
-#             begin
-#                 raise TriangleError
-#             end
-#         elsif one + two <= three || one + three <= two || three + two <= one
-#             begin
-#                 raise TriangleError
-#             end
-#         end
-#     end
-# end
-
